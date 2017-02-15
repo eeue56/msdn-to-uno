e4fbf4e66e246cda1d094d70a27b54571f5932ef
Noah <noah@fusetools.com>
2017-02-15 11:22:14 +0100

    take a url, replace types, grab constructors, make sure structs are correctly got

--- main.py
+++ main.py
@@ -3,26 +3,77 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy import signals
 import json
+import sys
 
 
 
 class MSDNSpider(scrapy.Spider):
     name = 'msdnspider'
-    start_urls = ['https://msdn.microsoft.com/en-us/library/system.drawing.drawing2d.graphicspath(v=vs.110).aspx']
+    start_urls = [
+        'https://msdn.microsoft.com/en-us/library/system.drawing.solidbrush(v=vs.110).aspx',
+        'https://msdn.microsoft.com/en-us/library/system.drawing.drawing2d.lineargradientbrush(v=vs.110).aspx',
+        'https://msdn.microsoft.com/en-us/library/system.drawing.texturebrush(v=vs.110).aspx'
+    ]
 
     def parse(self, response):
         for title in response.css('#content .topic .title'):
-            class_name = title.css("::text").extract_first().split()[0]
+            header_text = title.css("::text").extract_first().split()
+            class_name = header_text[0]
+            type = header_text[1]
+
+        
+        constructors = []
+        is_first = True
+
+        for constructor in response.css('#idConstructors tbody tr'):
+            if is_first:
+                is_first = False
+                continue
+
+            constructor_text = constructor.css("tr td a ::text").extract_first()
+            constructor_args = constructor.css("tr td a span ::text").extract_first().replace('\u2002', ' ')
+            constructors.append(constructor_text + constructor_args)
+
+
+        properties = []
+        is_first = True
+
+        for constructor in response.css('#idProperties tbody tr'):
+            if is_first:
+                is_first = False
+                continue
+
+            prop_text = constructor.css("tr td a span ::text").extract_first()
+            properties.append(prop_text)
 
         namespace = response.css('#mainBody .section a ::text').extract_first()
 
         yield { 
             'class_name': class_name,
-            'namespace': namespace
+            'namespace': namespace,
+            'type': type,
+            'constructors': constructors,
+            'properties': properties
         }
 
 
-def main():
+TYPE_TO_TEXT = { 
+    "Structure": "struct", 
+    "Class": "class"
+}
+
+TYPE_TO_UNO = {
+    "Single": "float"
+}
+
+def replace_types(thing):
+    for original_type, new_type in TYPE_TO_UNO.items():
+        thing.replace(original_type, new_type)
+
+    return thing
+
+
+def main(urls):
     try:
         with open('out.json') as f:
             with open('old.json', 'w') as old_f:
@@ -41,19 +92,45 @@ def main():
     })
 
 
-    process.crawl(MSDNSpider)
+    process.crawl(MSDNSpider, start_urls=urls)
     process.start()
 
     with open('out.json') as f:
         items = json.load(f)
 
     for item in items:
+        constructors = '\n\t'.join(
+            'public extern {constructor};'.format(
+                constructor=replace_types(constructor)
+            ) 
+            for constructor in item['constructors']
+        )
+
+        properties = '\n\t'.join(
+            'public extern {prop} {{ get; set; }}'.format(
+                prop=prop
+            ) 
+            for prop in item['properties']
+        )
+
         print("""
 [DotNetType("{namespace}.{class_name}")]
-extern(DOTNET) public class {class_name}
+extern(DOTNET) public {type} {class_name}
 {{
+\t// constructors
+\t{constructors}
+\n
+\t// properties
+\t{properties}
 }}
-        """.format(namespace=item['namespace'], class_name=item['class_name']))
+        """.format(
+                namespace=item['namespace'], 
+                class_name=item['class_name'],
+                type=TYPE_TO_TEXT[item['type']],
+                constructors=constructors,
+                properties=properties
+            )
+        )
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
